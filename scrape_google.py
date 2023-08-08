e9826b11b638463e0818fa1c3c2bb6f17918dee8
rikvegter <38559198+rikvegter@users.noreply.github.com>
2023-08-08 13:49:53 +0200

    Update scrape_google.py
    Maak een note als het aangeboden is

--- scrape_google.py
+++ scrape_google.py
@@ -5,7 +5,11 @@ from scrapfly import ScrapeConfig, ScrapflyClient
 
 scrapfly = ScrapflyClient("scp-live-c303a6f9a12f4b40913fa5df2c06bef5")
 
+fraud_sites = ['booking', 'airbnb', 'funda', 'marktplaats']
+#Dit is je exploitatiestand
+woningen = ['Elzenlaan 10']
 
+#Deze functie verwerkt het zoekwoord naar een gestructureerde datavorm voor verwerking
 def parse_search_results(selector: Selector):
     """parse search results from google search page"""
     results = []
@@ -19,8 +23,7 @@ def parse_search_results(selector: Selector):
         results.append((title, url, text))
     return results
 
-
-def scrape_search(query: str, page=1, country="US"):
+def scrape_search(query: str, page=1, country="NL"):
     """scrape search results for a given keyword"""
     # retrieve the SERP
     url = f"https://www.google.com/search?hl=en&q={quote(query)}" + (f"&start={10*(page-1)}" if page > 1 else "")
@@ -31,9 +34,18 @@ def scrape_search(query: str, page=1, country="US"):
     results["search"].extend(parse_search_results(result.selector))
     return dict(results)
 
-# Example use: scrape 3 pages: 1,2,3
-for page in [1]:
-    results = scrape_search("Elzenlaan 10", page=page)
-    for result in results["search"]:
-        print(result, '\n')
-        import pdb; pdb.set_trace()
+def act_on_fraud(fraud_site, woning):
+    print('Woning ', woning, ' wordt aangeboden op website ', fraud_site)
+
+
+#Loop over alle woningen in de eploitatiestand
+for woning in woningen:
+    #Loop over alle paginas van de google search
+    for page in [1, 2]:
+        results = scrape_search(woning, page=page)
+        #Loop over alle zoekresultaten heen
+        for result in results["search"]:
+            #Controleer of de fraude trefwoorden voorkomen in de url
+            for fraud_site in fraud_sites:
+                if fraud_site in result[1]:
+                    act_on_fraud(fraud_site, woning)
